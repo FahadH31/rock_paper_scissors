891969a140131287c19656f30cc050de69b38ace
FahadH31 <129327022+FahadH31@users.noreply.github.com>
2023-07-12 23:31:41 -0400

    Implemented "Computer Move" Display & Minor Fixes

--- rock-paper-scissors-script.js
+++ rock-paper-scissors-script.js
@@ -12,37 +12,37 @@ function playGame(userMove) {
     //Conditions for determining result.
     if (userMove === 'scissors') {
         if (computerMove === 'rock') {
-            result = 'lose.';
+            result = 'lost.';
         } else if (computerMove === 'paper') {
-            result = 'win.';
+            result = 'won.';
         } else if (computerMove === 'scissors') {
             result = 'tied.';
         }
     }
     else if (userMove === 'paper') {
         if (computerMove === 'rock') {
-            result = 'win.';
+            result = 'won.';
         } else if (computerMove === 'paper') {
             result = 'tied.';
         } else if (computerMove === 'scissors') {
-            result = 'lose.';
+            result = 'lost.';
         }
     }
     else if (userMove === 'rock') {
         if (computerMove === 'rock') {
             result = 'tied.';
         } else if (computerMove === 'paper') {
-            result = 'lose.';
+            result = 'lost.';
         } else if (computerMove === 'scissors') {
-            result = 'win.';
+            result = 'won.';
         }
     }
 
     //Calculating and saving the correct score based on the result.
-    if (result === 'win.') {
+    if (result === 'won.') {
         score.wins++;
     }
-    else if (result === 'lose.') {
+    else if (result === 'lost.') {
         score.losses++;
     }
     else if (result === 'tied.') {
@@ -65,13 +65,22 @@ function playGame(userMove) {
 function pickComputerMove() {
     const randomNumber = Math.random();
 
+    const computerMove = document.querySelector('.computer-move-button')
+    
+    //Show the image and caption for the computer move once this function runs.
+    computerMove.classList.remove('hide');
+    document.querySelector('.computer-move-caption').classList.remove('hide');
+
     if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerMove.innerHTML = '<img class = "computer-move-img" src = "rock.png">'; 
         return 'rock';
     }
     else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        computerMove.innerHTML = '<img class = "computer-move-img" src = "paper.png">'; 
         return 'paper';
     }
     else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+        computerMove.innerHTML = '<img class = "computer-move-img" src = "scissors.png">'; 
         return 'scissors';
     }
 }
@@ -80,8 +89,8 @@ function pickComputerMove() {
 function selectResultColor(result) {
     const resultText = document.querySelector('.js-result-text');
 
-    resultText.classList.toggle('green', result === 'win.')
-    resultText.classList.toggle('red', result === 'lose.')
+    resultText.classList.toggle('green', result === 'won.')
+    resultText.classList.toggle('red', result === 'lost.')
     resultText.classList.toggle('yellow', result === 'tied.')
 }
 
@@ -94,10 +103,14 @@ function printScore() {
 }
 
 function resetButtonClick() {
+    //Reset score and update immediately
     score.wins = score.losses = score.ties = 0;
     printScore();
     localStorage.removeItem('score')
 
+    //Hide the previous results 
+    document.querySelector('.computer-move-button').innerHTML = '<img class = "computer-move-img" src = "transparent.png">'; //This gets rid of only the image inside the button
     document.querySelector('.js-result-text').classList.add('hide');
     document.querySelector('.js-choices-text').classList.add('hide');
 }
+   
